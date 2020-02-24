98a68af1745e0c5c76bd0eff5a7932b195215a39
David37 <davidkondic@protonmail.com>
2020-02-24 23:59:25 +0200

    added test for price

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -18,5 +18,17 @@ namespace Tests
             //ASSERT
             IOrderMock.Received().SetDelivered(orderMock);
         }
+
+        [Test]
+        public void OrderPrice_Calculates_PriceFlower_Accurately(){
+            //ARRANGE
+            var IOrderMock=Substitute.For<IOrderDAO>();
+            var IClientMock=Substitute.For<IClient>();
+            var orderMock= new Order(IOrderMock,IClientMock); 
+            //ACT
+            //orderMock.Price.Returns(33);
+            //ASSERT
+            Assert.That(orderMock.Price,Is.EqualTo(33));
+        }
     }
 }
